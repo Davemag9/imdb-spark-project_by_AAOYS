e925159d824b795d8db4b5c8558b3c246436dd21
pavlivalina1 <comfy.it.224@gmail.com>
2025-04-04 20:38:31 +0300

    added schemas for title.akas and title.crew

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from data_loader import load_data
-from schemas import title_episode_schema, title_ratings_schema
+from schemas import title_episode_schema, title_ratings_schema, title_akas_schema, title_crew_schema
 
 title_ratings_df = load_data("./imdb_dataset/title.ratings.tsv", title_ratings_schema)
 title_episode_df = load_data("./imdb_dataset/title.episode.tsv", title_episode_schema)
+
+title_akas_df = load_data("./imdb_dataset/title.akas.tsv", title_akas_schema)
+title_crew_df = load_data("./imdb_dataset/title.crew.tsv", title_crew_schema)
--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType, BooleanType
 
 title_ratings_schema = StructType([
         StructField("tconst", StringType(), True),
@@ -13,3 +13,20 @@ title_episode_schema = StructType([
         StructField("seasonNumber", StringType(), True),
         StructField("episodeNumber", StringType(), True)
     ])
+
+title_akas_schema = StructType([
+    StructField("titleId", StringType(), True),
+    StructField("ordering", IntegerType(), True),
+    StructField("title", StringType(), True),
+    StructField("region", StringType(), True),
+    StructField("language", StringType(), True),
+    StructField("types", StringType(), True),       # буде строка з комами → можна потім розбити
+    StructField("attributes", StringType(), True),  # те ж саме
+    StructField("isOriginalTitle", StringType(), True)
+])
+
+title_crew_schema = StructType([
+    StructField("tconst", StringType(), True),
+    StructField("directors", StringType(), True),
+    StructField("writers", StringType(), True)
+])
