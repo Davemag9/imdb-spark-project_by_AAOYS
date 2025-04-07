2eadfb8d6b6e0054de62548d38c7332ca2fdf176
Oleh Lozovyi <davemag9@gmail.com>
2025-04-07 18:26:37 +0300

    add 2 more business cases

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split
 
-from src.business_cases import get_directors_worked_with_Hanks
+from src.business_cases import get_directors_worked_with_Hanks, get_top_rated_recent_films, get_most_common_actor_pairs
 from src.data_loader import load_data
 from src.schemas import title_episode_schema, title_ratings_schema, title_akas_schema, title_crew_schema, \
     title_basics_schema, title_principals_schema, name_basics_schema
@@ -28,4 +28,12 @@ title_crew_df = title_crew_df.withColumn("directors", split(title_crew_df["direc
 title_crew_df = title_crew_df.withColumn("writers", split(title_crew_df["writers"], ","))
 
 # business cases
-get_directors_worked_with_Hanks(name_basics_df, title_principals_df, title_basics_df, title_crew_df)
+#ALINA
+
+#ANASTASIA
+
+#OLEH
+# get_directors_worked_with_Hanks(name_basics_df, title_principals_df, title_basics_df, title_crew_df) # 2m
+# get_top_rated_recent_films(title_basics_df, title_ratings_df) # 1m
+# get_most_common_actor_pairs(title_principals_df, title_basics_df, name_basics_df) # 6m
+#YURA
--- src/business_cases.py
+++ src/business_cases.py
@@ -1,4 +1,5 @@
 from pyspark.sql.functions import explode, count, col
+from pyspark.sql import functions as F
 
 from src.get_services import get_id_by_name, get_films_by_actor
 
@@ -25,3 +26,49 @@ def get_directors_worked_with_Hanks(name_basics_df, title_principals_df, title_b
 
     result.show(10, truncate=False)
     return result
+
+
+def get_top_rated_recent_films(title_basics_df, title_ratings_df):
+    result = title_basics_df.join(title_ratings_df, "tconst") \
+        .filter((col("averageRating") > 8.0) & (col("startYear") > 2010)) \
+        .select("primaryTitle", "startYear", "averageRating") \
+        .orderBy(col("averageRating").desc())
+
+    result.show(10, truncate=False)
+    return result
+
+
+
+def get_most_common_actor_pairs(title_principals_df, title_basics_df, name_basics_df):
+    movies = title_basics_df.filter(col("titleType") == "movie").select("tconst")
+
+    actors = title_principals_df \
+        .filter((col("category").isin("actor", "actress")) & (col("characters").isNotNull())) \
+        .join(movies, "tconst") \
+        .select("tconst", "nconst")
+
+    actor_pairs = actors.alias("a1") \
+        .join(actors.alias("a2"),
+              (col("a1.tconst") == col("a2.tconst")) & (col("a1.nconst") < col("a2.nconst"))) \
+        .select(
+            col("a1.nconst").alias("actor1"),
+            col("a2.nconst").alias("actor2")
+        )
+
+    pair_counts = actor_pairs.groupBy("actor1", "actor2") \
+        .agg(count("*").alias("num_movies_together"))
+
+    pair_with_names = pair_counts \
+        .join(name_basics_df.select("nconst", "primaryName").alias("n1"),
+              pair_counts.actor1 == F.col("n1.nconst")) \
+        .join(name_basics_df.select("nconst", "primaryName").alias("n2"),
+              pair_counts.actor2 == F.col("n2.nconst")) \
+        .select(
+            F.col("n1.primaryName").alias("Actor 1"),
+            F.col("n2.primaryName").alias("Actor 2"),
+            "num_movies_together"
+        ) \
+        .orderBy(col("num_movies_together").desc())
+
+    pair_with_names.show(10, truncate=False)
+    return pair_with_names
