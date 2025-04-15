47260974c0aba3d71084020bc0a906c90c0fb6be
Анастасія Кудибовська <Kudibovskanastya373@gmail.com>
2025-04-16 01:49:42 +0300

    fixed missing values

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import split, when, col
 
 from src.business_cases import get_directors_worked_with_Hanks, get_episodes_summary_per_season, get_top_3_movies_per_genre, \
     get_top_actors_by_high_rated_movies_count, get_top_countries_with_high_rated_movies, get_top_genres_by_avg_rating_last_10_years, \
@@ -37,26 +38,35 @@ title_crew_df = title_crew_df.withColumn("writers", split(title_crew_df["writers
 title_basics_df = title_basics_df.withColumn("genres", split(title_basics_df["genres"], ","))
 title_akas_df = title_akas_df.withColumn("types", split(title_akas_df["types"], ","))
 title_akas_df = title_akas_df.withColumn("attributes", split(title_akas_df["attributes"], ","))
+# title_episode_df = title_episode_df.withColumn("seasonNumber", when(col("seasonNumber") == "\\N", -1).otherwise(col("seasonNumber").cast(IntegerType())))
+# title_episode_df = title_episode_df.withColumn("episodeNumber", when(col("episodeNumber") == "\\N", -1).otherwise(col("episodeNumber").cast(IntegerType())))
 
 # Dataset analysis
-# show_data_summary(title_basics_df, "Title Basics DataFrame", extended_stats=True)
+show_data_summary(title_ratings_df, "Title Ratings DataFrame", extended_stats=True)
+show_data_summary(title_episode_df, "Title Episode DataFrame", extended_stats=True)
+show_data_summary(title_akas_df, "Title Akas DataFrame", extended_stats=True)
+show_data_summary(title_crew_df, "Title Crew DataFrame", extended_stats=True)
+show_data_summary(title_basics_df, "Title Basics DataFrame", extended_stats=True)
+show_data_summary(name_basics_df, "Name Basics DataFrame", extended_stats=True)
+show_data_summary(title_principals_df, "Title Principals DataFrame", extended_stats=True)
+
 
 # business cases
 #ALINA
-long_high_rated_movies_df = get_long_high_rated_movies(title_basics_df, title_ratings_df)
-successful_directors_df = get_successful_directors(name_basics_df, title_basics_df, title_ratings_df, title_crew_df)
-avg_runtime_by_genre_df = get_avg_runtime_by_genre(title_basics_df)
-count_good_movies_by_year_df = count_good_movies_by_year(title_basics_df, title_ratings_df)
-bottom_3_by_year_df = get_bottom_3_by_year(title_basics_df, title_ratings_df)
-actors_with_strong_debut_df = get_actors_with_strong_debut(name_basics_df, title_principals_df, title_basics_df, title_ratings_df)
+# long_high_rated_movies_df = get_long_high_rated_movies(title_basics_df, title_ratings_df)
+# successful_directors_df = get_successful_directors(name_basics_df, title_basics_df, title_ratings_df, title_crew_df)
+# avg_runtime_by_genre_df = get_avg_runtime_by_genre(title_basics_df)
+# count_good_movies_by_year_df = count_good_movies_by_year(title_basics_df, title_ratings_df)
+# bottom_3_by_year_df = get_bottom_3_by_year(title_basics_df, title_ratings_df)
+# actors_with_strong_debut_df = get_actors_with_strong_debut(name_basics_df, title_principals_df, title_basics_df, title_ratings_df)
 
 #ANASTASIIA
-top_genres_by_avg_rating_last_10_years_df = get_top_genres_by_avg_rating_last_10_years(title_basics_df, title_ratings_df)
-top_countries_with_high_rated_movies_df = get_top_countries_with_high_rated_movies(title_basics_df, title_akas_df, title_ratings_df, country_codes_df)
-top_3_movies_per_genre_df = get_top_3_movies_per_genre(title_basics_df, title_ratings_df)
-top_actors_by_high_rated_movies_count_df = get_top_actors_by_high_rated_movies_count(title_basics_df, title_principals_df, title_ratings_df, name_basics_df)
-episodes_summary_per_season_df = get_episodes_summary_per_season(title_episode_df, title_ratings_df, title_basics_df)
-top_movies_runtime_per_genre_df = get_top_movies_runtime_per_genre(title_basics_df, title_ratings_df)
+# top_genres_by_avg_rating_last_10_years_df = get_top_genres_by_avg_rating_last_10_years(title_basics_df, title_ratings_df)
+# top_countries_with_high_rated_movies_df = get_top_countries_with_high_rated_movies(title_basics_df, title_akas_df, title_ratings_df, country_codes_df)
+# top_3_movies_per_genre_df = get_top_3_movies_per_genre(title_basics_df, title_ratings_df)
+# top_actors_by_high_rated_movies_count_df = get_top_actors_by_high_rated_movies_count(title_basics_df, title_principals_df, title_ratings_df, name_basics_df)
+# episodes_summary_per_season_df = get_episodes_summary_per_season(title_episode_df, title_ratings_df, title_basics_df)
+# top_movies_runtime_per_genre_df = get_top_movies_runtime_per_genre(title_basics_df, title_ratings_df)
 
 # show_data_summary(top_genres_by_avg_rating_last_10_years_df, "Top_genres_by_avg_rating_last_10_years_df")    
                                    
@@ -79,20 +89,20 @@ entries_per_film_df = count_entries_per_film(title_principals_df, title_basics_d
 
 # write data to csv
 #ALINA
-write_data_to_csv(long_high_rated_movies_df, "./results/long_high_rated_movies.csv")
-write_data_to_csv(successful_directors_df, "./results/successful_directors.csv")
-write_data_to_csv(avg_runtime_by_genre_df, "./results/avg_runtime_by_genre.csv")
-write_data_to_csv(count_good_movies_by_year_df, "./results/count_good_movies_by_year.csv")
-write_data_to_csv(bottom_3_by_year_df, "./results/bottom_3_by_year.csv")
-write_data_to_csv(actors_with_strong_debut_df, "./results/actors_with_strong_debut.csv")
+# write_data_to_csv(long_high_rated_movies_df, "./results/long_high_rated_movies.csv")
+# write_data_to_csv(successful_directors_df, "./results/successful_directors.csv")
+# write_data_to_csv(avg_runtime_by_genre_df, "./results/avg_runtime_by_genre.csv")
+# write_data_to_csv(count_good_movies_by_year_df, "./results/count_good_movies_by_year.csv")
+# write_data_to_csv(bottom_3_by_year_df, "./results/bottom_3_by_year.csv")
+# write_data_to_csv(actors_with_strong_debut_df, "./results/actors_with_strong_debut.csv")
 
 #ANASTASIIA
-write_data_to_csv(top_genres_by_avg_rating_last_10_years_df, "./results/top_genres_by_avg_rating_last_10_years.csv")
-write_data_to_csv(top_countries_with_high_rated_movies_df, "./results/top_countries_with_high_rated_movies.csv")
-write_data_to_csv(top_3_movies_per_genre_df, "./results/top_3_movies_per_genre.csv")
-write_data_to_csv(top_actors_by_high_rated_movies_count_df, "./results/top_actors_by_high_rated_movies_count.csv")
-write_data_to_csv(episodes_summary_per_season_df, "./results/get_episodes_summary_per_season.csv")
-write_data_to_csv(top_movies_runtime_per_genre_df, "./results/top_movies_runtime_per_genre.csv")
+# write_data_to_csv(top_genres_by_avg_rating_last_10_years_df, "./results/top_genres_by_avg_rating_last_10_years.csv")
+# write_data_to_csv(top_countries_with_high_rated_movies_df, "./results/top_countries_with_high_rated_movies.csv")
+# write_data_to_csv(top_3_movies_per_genre_df, "./results/top_3_movies_per_genre.csv")
+# write_data_to_csv(top_actors_by_high_rated_movies_count_df, "./results/top_actors_by_high_rated_movies_count.csv")
+# write_data_to_csv(episodes_summary_per_season_df, "./results/get_episodes_summary_per_season.csv")
+# write_data_to_csv(top_movies_runtime_per_genre_df, "./results/top_movies_runtime_per_genre.csv")
 
 #OLEH
 # write_data_to_csv(directors_worked_with_Hanks_df, "./results/directors_worked_with_Hanks.csv")
